674aee8718972b5ef5ae2c60e2c26431280a82b1
hyeonnjii <114765085+hyeonnjii@users.noreply.github.com>
2024-01-15 18:53:41 +0900

    feat: Generate 100 prisoners
    Each prisoner gets own number and 'found' flag key with default boolean
    False.

--- prisoners.py
+++ prisoners.py
@@ -1,1 +1,7 @@
-print('hello')
+import random
+
+def generate_prisoners():
+    return [{'number': i, 'found': False} for i in range(1, 101)]
+
+if __name__=='__main__':
+    print(generate_prisoners())
